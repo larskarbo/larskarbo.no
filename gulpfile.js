439c34f28319eeeb8b67f9e7ddb71e9ca1d0a1ef
lars <larskarbo@hotmail.com>
2017-09-10 15:34:48 +0200

    complete rewrite

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var uglify = require('gulp-uglify')
+var minifyCss = require('gulp-minify-css')
+
+gulp.task('sass', function() {
+	return gulp
+		.src('paper/styles.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', function() {
+	gulp.start('sass')
+	gulp.watch('paper/*.*', ['sass']);
+});
